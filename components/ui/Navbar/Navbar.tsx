2a56ca610923c872f97026b112f1c5c7147c85c2
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-10-11 21:44:11 +0200

    feat: add tablet media queries for Navbar component

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -18,28 +18,28 @@ function Navbar() {
           <li className={styles.navItem}>
             <Link href="/">
               <a>
-                <b>00</b> home
+                <b className={styles.navItemBold}>00</b> home
               </a>
             </Link>
           </li>
           <li className={styles.navItem}>
             <Link href="/destination/moon">
               <a>
-                <b>01</b> destination
+                <b className={styles.navItemBold}>01</b> destination
               </a>
             </Link>
           </li>
           <li className={styles.navItem}>
             <Link href="/crew/douglas_hurley">
               <a>
-                <b>02</b> crew
+                <b className={styles.navItemBold}>02</b> crew
               </a>
             </Link>
           </li>
           <li className={styles.navItem}>
             <Link href="/technology/launch_vehicle">
               <a>
-                <b>03</b> technology
+                <b className={styles.navItemBold}>03</b> technology
               </a>
             </Link>
           </li>
