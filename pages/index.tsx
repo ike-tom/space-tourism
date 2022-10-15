c9931e68f6d5c6f73217cf445205d44f3552daff
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-10-15 11:12:41 +0200

    feat: add phone media queries for HomePage

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,20 +10,22 @@ const Home: NextPage = () => {
       <Background alt="earth" url="/home/background-home-desktop.jpg" />
       <main className={styles.main}>
         <Navbar />
-        <div className={styles.textWrapper}>
-          <h2 className={styles.subHeader}>so, you want to travel to</h2>
-          <h1 className={styles.header}>space</h1>
-          <p className={styles.description}>
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience!
-          </p>
-        </div>
-        <div className={styles.exploreWrapper}>
-          <Link href="/destination/moon">
-            <a className={styles.exploreHeader}>explore</a>
-          </Link>
+        <div className={styles.textButtonContainer}>
+          <div className={styles.textWrapper}>
+            <h2 className={styles.subHeader}>so, you want to travel to</h2>
+            <h1 className={styles.header}>space</h1>
+            <p className={styles.description}>
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+          <div className={styles.exploreWrapper}>
+            <Link href="/destination/moon">
+              <a className={styles.exploreHeader}>explore</a>
+            </Link>
+          </div>
         </div>
       </main>
     </>
